da318f5abb58264ff55b322709b8968ba93ae9f1
Guilherme Marzano <guilhermemarzano@gmail.com>
2023-12-12 23:59:06 -0300

    Organized data

--- func_compra.py
+++ func_compra.py
@@ -78,72 +78,75 @@ def atualizar_produtos(lst_compra:list)-> list:
     prod_nome = ''
     flg_chg = True
 
-    prod_nome = input('Digite nome produto para alterar: ').lower()
-    if existe_produto_lista(prod_nome, lst_compra):
-        prod_pos = pegar_produto_posicao_lista(prod_nome,lst_compra)
-        prod_inf = lst_compra[prod_pos].copy()
-        i = 0
-        while flg_chg:
-            if i < 3:
-                try:
-                    flg_atualiza = int(input(f'Deseja atualizar {prod_dados[i]} (1)sim, (2)não , (3)abortar: '))
-                    if flg_atualiza == 1:
-                        match i:
-                            case 0:
-                                novo_nome = input('Digite o nome do produto: ').lower()
-                                if len(novo_nome) < 1:
-                                    print('Nome do produto deve ter mais que 1 caracter')
-                                else:
-                                    if existe_produto_lista(novo_nome,lst_compra):
-                                        print(f'Novo nome escolhido já existe na lista.\
-                                              \nNome atual: {lst_compra[prod_pos]["produto"]}   será mantido')
-                                        novo_nome = lst_compra[prod_pos]['produto']
-                            case 1:
-                                novo_valor = aquisitar_numeros('Digite valor produto: ')
-                                if novo_valor[0] == False:
-                                    print('Atualização de produto abortada')
-                                    flg_atualiza = 3
-                                    flg_chg = False
-                            case 2:
-                                nova_qte = aquisitar_numeros('Digite a quantidade de produto: ')
-                                if nova_qte[0] == False:
-                                    print('Atualização de produto abortada')
-                                    flg_atualiza = 3
-                                    flg_chg = False
-                        i +=1
-                    elif flg_atualiza == 2:
-                        match i:
-                            case 0:
-                                novo_nome = prod_nome
-                            case 1:
-                                #Uma tupla pois é o mesmo padrão de retorno da função aquisitar_numeros
-                                novo_valor =(True, prod_inf['valor']) 
-                            case 2:
-                                #Uma tupla pois é o mesmo padrão de retorno da função aquisitar_numeros
-                                nova_qte = (True, prod_inf['quantidade'])
-                        i +=1
-                    elif flg_atualiza == 3:
-                        print('Atualização de produto abortada')
-                        flg_chg = False
-                    else:
-                        print('Digitar apenas (1)sim,(2)nâo e (3) para abortar')
-
-                except ValueError:
-                    print('Digite apenas 1 para sim\n 2 para não\n3 para abortar')
-
-            else:
-                flg_atualiza = 4 #Significa que a atualização foi concluida
-                flg_chg = False
-        if flg_atualiza == 4: #Alteração concluida
-            novo_total = calc_prod_total(novo_valor[1],nova_qte[1])
-            prod_inf.update({'produto': novo_nome,'valor':novo_valor[1],'quantidade':nova_qte[1],'total': novo_total})
-            lst_compra[prod_pos] = prod_inf
-            return lst_compra
-        elif flg_atualiza == 3:
-            return lst_compra
+    if len(lst_compra)==0:
+        print('Lista de compra está vazia, adicione um produto antes de alterar')
     else:
-        print('Produto desejado não existe na lista, escolha a opção adicionar')
-        return lst_compra
+        prod_nome = input('Digite nome produto para alterar: ').lower()
+        if existe_produto_lista(prod_nome, lst_compra):
+            prod_pos = pegar_produto_posicao_lista(prod_nome,lst_compra)
+            prod_inf = lst_compra[prod_pos].copy()
+            i = 0
+            while flg_chg:
+                if i < 3:
+                    try:
+                        flg_atualiza = int(input(f'Deseja atualizar {prod_dados[i]} (1)sim, (2)não , (3)abortar: '))
+                        if flg_atualiza == 1:
+                            match i:
+                                case 0:
+                                    novo_nome = input('Digite o nome do produto: ').lower()
+                                    if len(novo_nome) < 1:
+                                        print('Nome do produto deve ter mais que 1 caracter')
+                                    else:
+                                        if existe_produto_lista(novo_nome,lst_compra):
+                                            print(f'Novo nome escolhido já existe na lista.\
+                                                \nNome atual: {lst_compra[prod_pos]["produto"]}   será mantido')
+                                            novo_nome = lst_compra[prod_pos]['produto']
+                                case 1:
+                                    novo_valor = aquisitar_numeros('Digite valor produto: ')
+                                    if novo_valor[0] == False:
+                                        print('Atualização de produto abortada')
+                                        flg_atualiza = 3
+                                        flg_chg = False
+                                case 2:
+                                    nova_qte = aquisitar_numeros('Digite a quantidade de produto: ')
+                                    if nova_qte[0] == False:
+                                        print('Atualização de produto abortada')
+                                        flg_atualiza = 3
+                                        flg_chg = False
+                            i +=1
+                        elif flg_atualiza == 2:
+                            match i:
+                                case 0:
+                                    novo_nome = prod_nome
+                                case 1:
+                                    #Uma tupla pois é o mesmo padrão de retorno da função aquisitar_numeros
+                                    novo_valor =(True, prod_inf['valor']) 
+                                case 2:
+                                    #Uma tupla pois é o mesmo padrão de retorno da função aquisitar_numeros
+                                    nova_qte = (True, prod_inf['quantidade'])
+                            i +=1
+                        elif flg_atualiza == 3:
+                            print('Atualização de produto abortada')
+                            flg_chg = False
+                        else:
+                            print('Digitar apenas (1)sim,(2)nâo e (3) para abortar')
+
+                    except ValueError:
+                        print('Digite apenas 1 para sim\n 2 para não\n3 para abortar')
+
+                else:
+                    flg_atualiza = 4 #Significa que a atualização foi concluida
+                    flg_chg = False
+            if flg_atualiza == 4: #Alteração concluida
+                novo_total = calc_prod_total(novo_valor[1],nova_qte[1])
+                prod_inf.update({'produto': novo_nome,'valor':novo_valor[1],'quantidade':nova_qte[1],'total': novo_total})
+                lst_compra[prod_pos] = prod_inf
+                return lst_compra
+            elif flg_atualiza == 3:
+                return lst_compra
+        else:
+            print('Produto desejado não existe na lista, escolha a opção adicionar')
+            return lst_compra
 
 def remover_produto(lst_compra:list) -> list:
     print('-'*30)
@@ -152,28 +155,35 @@ def remover_produto(lst_compra:list) -> list:
     prod_nome = ''
     flg_remove = 0
     nw_lst = []
-    while flg_remove == 0:
-        prod_nome = input('Digite nome produto para alterar ou (s) para sair: ').lower()
-        if prod_nome == 's':
-            print("Função abortada")
-            flg_remove = -1
-        else:
-            if existe_produto_lista(prod_nome, lst_compra):
-                prod_pos = pegar_produto_posicao_lista(prod_nome,lst_compra)
-                i = 0
-                for item in lst_compra:
-                    if i != prod_pos:
-                        nw_lst.append(lst_compra[i])
-                    i +=1
-                print('*'*30)
-                print(f'Produto {prod_nome} removido')
-                flg_remove = 1
 
-            else:
-                print('Produto não existe na lista')
-                flg_remove = -1
-    if flg_remove == 1:
-        return nw_lst
+    if len(lst_compra) == 0:
+        print('Lista de compras está vazia')
     else:
-        return lst_compra
+        while flg_remove == 0:
+            prod_nome = input('Digite nome produto para alterar ou (s) para sair: ').lower()
+            if prod_nome == 's':
+                print("Função abortada")
+                flg_remove = -1
+            else:
+                if existe_produto_lista(prod_nome, lst_compra):
+                    prod_pos = pegar_produto_posicao_lista(prod_nome,lst_compra)
+                    i = 0
+                    for item in lst_compra:
+                        if i != prod_pos:
+                            nw_lst.append(lst_compra[i])
+                        i +=1
+                    print('*'*30)
+                    print(f'Produto {prod_nome} removido')
+                    flg_remove = 1
+
+                else:
+                    print('Produto não existe na lista')
+                    flg_remove = -1
+        if flg_remove == 1:
+            return nw_lst
+        else:
+            return lst_compra
+    
+if __name__ == "__main__":
+    print('Este é um módulo e não deve ser executado diretamente.')
             
--- main_prod_compra.py
+++ main_prod_compra.py
@@ -7,46 +7,48 @@ from func_compra import(
 
 from func_auxiliares import calc_total_compra
 
-lst_compra = []
-flg_prog = True
-flg_oper = 0
+def init():
+    lst_compra = []
+    flg_prog = True
+    flg_oper = 0
 
-while flg_prog:
-    print('-'*30)    
-    try:
-        print(
-              'Digite a operação desejada:\
-              \n1:Adicionar produto\
-              \n2:Listar itens compra\
-              \n3:Alterar produto\
-              \n4:Remover produto\
-              \n5:Finalizar programa'
-             )
-        flg_oper = int(input('Digite sua opção: '))
-    except ValueError:
-        print('Digitar apenas uma das opções indicadas')
-        flg_oper = 0
-    match flg_oper:
-        case 1:
-            lst_compra = adicionar_produtos(lst_compra)
-        case 2:
-            ver_lista_compra(lst_compra)
-            print('='*10)
-            print(f'Total compra = {calc_total_compra(lst_compra)}')
-        case 3:
-            lst_compra = atualizar_produtos(lst_compra)
-        case 4:
-            lst_compra = remover_produto(lst_compra)
-        case 5:
-            print('Lista compra finalizada\n')
-            flg_prog = False
+    while flg_prog:
+        print('-'*30)    
+        try:
+            print(
+                'Digite a operação desejada:\
+                \n1:Adicionar produto\
+                \n2:Listar itens compra\
+                \n3:Alterar produto\
+                \n4:Remover produto\
+                \n5:Finalizar programa'
+                )
+            flg_oper = int(input('Digite sua opção: '))
+        except ValueError:
+            print('Digitar apenas uma das opções indicadas')
+            flg_oper = 0
+        match flg_oper:
+            case 1:
+                lst_compra = adicionar_produtos(lst_compra)
+            case 2:
+                ver_lista_compra(lst_compra)
+                print('='*10)
+                print(f'Total compra = {calc_total_compra(lst_compra)}')
+            case 3:
+                lst_compra = atualizar_produtos(lst_compra)
+            case 4:
+                lst_compra = remover_produto(lst_compra)
+            case 5:
+                print('Lista compra finalizada\n')
+                flg_prog = False
 
-if len(lst_compra) == 0:
-    print("Programa abortado")
-else:
-    ver_lista_compra(lst_compra)
-    print('='*10)
-    print(f'Total compra = {calc_total_compra(lst_compra)}')
+    if len(lst_compra) == 0:
+        print("Programa abortado")
+    else:
+        ver_lista_compra(lst_compra)
+        print('='*10)
+        print(f'Total compra = {calc_total_compra(lst_compra)}')
 
-
+if __name__ == "__main__":
+    init()
 
